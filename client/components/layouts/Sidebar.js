99a357a733b380bc447bff4254a5896dc26c56ea
Ltwoz <warrapat.102@gmail.com>
2023-07-14 18:14:44 +0700

    Sidebar content

--- client/components/layouts/Sidebar.js
+++ client/components/layouts/Sidebar.js
@@ -4,9 +4,34 @@ import { useSidebar } from "@/contexts/sidebar-context";
 import { useEffect } from "react";
 import Image from "next/image";
 import { IoMdClose } from "react-icons/io";
+import { useRouter } from "next/router";
+
+const Menus = [
+    {
+        name: "หน้าหลัก",
+        href: `/`,
+    },
+    {
+        name: "เกี่ยวกับ",
+        href: `/about`,
+    },
+    {
+        name: "สินค้าของเรา",
+        href: `/products`,
+    },
+    {
+        name: "ลานทองพาเที่ยว",
+        href: `/reviews`,
+    },
+    {
+        name: "ติดต่อ",
+        href: `/contact`,
+    },
+];
 
 const Sidebar = () => {
     const { isOpen, setIsOpen } = useSidebar();
+    const router = useRouter();
 
     // useEffect(() => {
     //     const menuHandler = () => setIsOpen(false);
@@ -55,13 +80,38 @@ const Sidebar = () => {
                                 className="object-cover"
                             />
                         </div>
-                        <h2 className="text-lg font-semibold text-[#E32C2C]">หจก.ลานทองเชียงใหม่</h2>
+                        <h2 className="text-lg font-semibold text-[#E32C2C]">
+                            หจก.ลานทองเชียงใหม่
+                        </h2>
                     </Link>
                     <IoMdClose
                         onClick={() => setIsOpen(!isOpen)}
                         className="w-6 h-6"
                     />
                 </div>
+                <ul id="sidebar-items" className="mt-6">
+                    {Menus.map((menu, i) => {
+                        const isActive = router.asPath === menu.href;
+                        return (
+                            <li
+                                key={i}
+                                className={`rounded hover:bg-zinc-100/80 text-zinc-900 ${
+                                    isActive ? "bg-gray-100/80" : ""
+                                }`}
+                                onClick={() => setIsOpen(false)}
+                            >
+                                <Link
+                                    href={menu.href}
+                                    className="flex items-center px-4 py-3"
+                                >
+                                    <div className="inline-flex items-center font-medium">
+                                        {menu.name}
+                                    </div>
+                                </Link>
+                            </li>
+                        );
+                    })}
+                </ul>
             </motion.div>
         </AnimatePresence>
     );
