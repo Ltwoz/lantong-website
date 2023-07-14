41f696648aef973955391ef5c0d17a39078ab7a2
Ltwoz <warrapat.102@gmail.com>
2023-07-14 11:24:36 +0700

    basic setup

---
+++ client/components/layouts/Layout.js
@@ -0,0 +1,37 @@
+import Head from "next/head";
+import Navbar from "./Navbar";
+
+const Layout = ({ children, isDashboard }) => {
+    return (
+        <>
+            <Head>
+                <title>หจก.ลานทองเชียงใหม่</title>
+                <meta name="description" content="" />
+                <meta
+                    name="keywords"
+                    content="พ่วงข้าง, ลานทอง, ขายรถเชียงใหม่"
+                />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
+                <meta property="og:type" content="website" />
+                <meta property="og:title" content="Lantong Shop" />
+                <meta property="og:description" content="" />
+                <meta property="og:image" content="" />
+                <link rel="icon" href="" />
+            </Head>
+            <div
+                id="page-wrapper"
+                className={`min-h-screen flex flex-col text-gray-900`}
+            >
+                <>
+                    <Navbar />
+                    <div className="flex-grow">{children}</div>
+                </>
+            </div>
+        </>
+    );
+};
+
+export default Layout;
