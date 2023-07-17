5908e9a5f1d042f5a4a7b528f76c148076b1c4a6
Ltwoz <warrapat.102@gmail.com>
2023-07-17 23:30:32 +0700

    fetch products from api

--- client/pages/products/index.js
+++ client/pages/products/index.js
@@ -1,18 +1,42 @@
 import ProductCard from "@/components/ui/products/ProductCard";
 import Layout from "@/components/layouts/Layout";
 import ProductFilter from "@/components/ui/products/ProductFilter";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-export default function CatalogPage() {
+export default function ProductsPage() {
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    const [link, setLink] = useState(`/api/products?isActive=true`);
+
+    useEffect(() => {
+        const getProducts = async () => {
+            const { data } = await axios.get(`http://localhost:8000${link}`);
+            setProducts(data?.products);
+            setLoading(false);
+        };
+
+        getProducts().catch(() => {
+            console.error;
+            setLoading(false);
+        });
+
+        console.log("done");
+    }, [link]);
+
     return (
         <Layout>
             <div className="mx-auto max-w-[1200px] px-4 md:px-0 flex flex-col md:flex-row md:space-x-6">
                 <ProductFilter />
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-3 md:gap-6 mt-4 md:mt-0">
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard /> 
+                    {products?.length !== 0 ? (
+                        products.map((product) => (
+                            <ProductCard product={product} />
+                        ))
+                    ) : (
+                        <div className="text-center">Product not found.</div>
+                    )}
                 </div>
             </div>
         </Layout>
