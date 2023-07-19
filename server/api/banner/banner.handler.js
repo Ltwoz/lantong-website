6ff83f8bb448e7d73128119ab3dd18a393430508
Ltwoz <warrapat.102@gmail.com>
2023-07-19 22:04:51 +0700

    banner upload image

--- server/api/banner/banner.handler.js
+++ server/api/banner/banner.handler.js
@@ -1,8 +1,34 @@
 const catchAsyncErrors = require("../../middleware/catchAsyncErrors");
 const Banner = require("./banner.model");
+const multer = require("multer");
+const { promisify } = require("util");
+const { uploadFile } = require("../../utils/s3");
+
+// Create a multer instance and configure it
+const storage = multer.memoryStorage();
+const uploadMiddleware = multer({ storage: storage }).array("images");
+const uploadMiddlewareAsync = promisify(uploadMiddleware);
 
 // Create Banner
 exports.CreateBanner = catchAsyncErrors(async (req, res, next) => {
+    await uploadMiddlewareAsync(req, res);
+
+    const files = req.files;
+    const result = await uploadFile(files);
+
+    const updateImages = [];
+
+    for (let i = 0; i < result.length; i++) {
+        updateImages.push({
+            public_id: result[i].key,
+            url: result[i].Location,
+        });
+    }
+
+    req.body.images = updateImages;
+
+    const banner = await Banner.create(req.body);
+
     res.status(201).json({ success: true, image: "image" });
 });
 
