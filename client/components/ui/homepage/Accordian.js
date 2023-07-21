303f9096638f7327241e740474c5bd13b379293d
Ltwoz <warrapat.102@gmail.com>
2023-07-21 23:15:55 +0700

    edit accordian classes

--- client/components/ui/homepage/Accordian.js
+++ client/components/ui/homepage/Accordian.js
@@ -9,21 +9,21 @@ const Accordian = ({ title, content }) => {
 
     return (
         <div
-            className="cursor-pointer border-b border-[#CBD5E0]"
+            className="cursor-pointer border-b border-[#CBD5E0] py-2"
             onClick={toggleExpanded}
         >
-            <div className="px-6 text-left items-center h-20 select-none flex justify-between flex-row">
+            <div className="px-6 text-left items-center h-10 select-none flex justify-between flex-row">
                 <h5 className="flex-1">{title}</h5>
                 <div className="flex-none pl-2">
                     {expanded ? minusIcon : plusIcon}
                 </div>
             </div>
             <div
-                className={`px-6 pt-0 overflow-hidden transition-[max-height] duration-300 ease-in ${
+                className={`px-6 pt-0 overflow-y-scroll md:overflow-hidden transition-[max-height] duration-300 ease-in ${
                     expanded ? "max-h-40" : "max-h-0"
                 }`}
             >
-                <p className="pb-4 text-left">{content}</p>
+                <p className="pb-2 text-left">{content}</p>
             </div>
         </div>
     );
