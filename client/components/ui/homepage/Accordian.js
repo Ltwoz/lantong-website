c18167aaddc5e20b0313260228512f1b1690b389
Ltwoz <warrapat.102@gmail.com>
2023-07-22 12:24:46 +0700

    change icon to chevron

--- client/components/ui/homepage/Accordian.js
+++ client/components/ui/homepage/Accordian.js
@@ -4,9 +4,6 @@ const Accordian = ({ title, content }) => {
     const [expanded, setExpanded] = useState(false);
     const toggleExpanded = () => setExpanded((current) => !current);
 
-    const minusIcon = "-";
-    const plusIcon = "+";
-
     return (
         <div
             className="cursor-pointer border-b border-[#CBD5E0] py-2"
@@ -14,8 +11,24 @@ const Accordian = ({ title, content }) => {
         >
             <div className="px-6 text-left items-center h-10 select-none flex justify-between flex-row">
                 <h5 className="flex-1">{title}</h5>
-                <div className="flex-none pl-2">
-                    {expanded ? minusIcon : plusIcon}
+                <div className={`flex-none transition-all duration-300 ${expanded ? "-rotate-180" : "rotate-0"}`}>
+                    <svg
+                        stroke="currentColor"
+                        fill="currentColor"
+                        strokeWidth="0"
+                        viewBox="0 0 512 512"
+                        height="1em"
+                        width="1em"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                            fill="none"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="48"
+                            d="M112 184l144 144 144-144"
+                        />
+                    </svg>
                 </div>
             </div>
             <div
