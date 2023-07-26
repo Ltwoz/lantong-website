58c2d41fee1f9c6fc924cbb60429f0f0c9aac9d8
JarYin <tempest353@hotmail.com>
2023-07-26 22:10:31 +0700

    update responsive for phone in contact folder

--- client/pages/contact/index.js
+++ client/pages/contact/index.js
@@ -21,8 +21,8 @@ export default function ContactUs() {
         <div className="md:w-[1200px] border mx-auto w-[375px]">
           {/* Facebook */}
           <Link href="#">
-            <div className="bg-[#4267B2] h-[300px] md:flex items-center mt-[125px] ">
-              <BsFacebook className="md:w-[200px] md:h-[200px] md:ml-[56px] ml-auto mr-auto w-[100px] h-[100px]" />
+            <div className="bg-[#4267B2] md:h-[300px] h-fit md:flex items-center md:mt-[125px] mt-[35px] p-5 ">
+              <BsFacebook className="md:w-[200px] md:h-[200px] md:ml-[56px] max-[376px]:ml-auto max-[376px]:mr-auto w-[100px] h-[100px]" />
               <h1 className="text-white md:text-[74px] md:ml-[182px] text-[24px] text-center">
                 ลานทองพ่วงข้าง
               </h1>
@@ -31,8 +31,8 @@ export default function ContactUs() {
 
           {/* Line */}
           <Link href="#">
-            <div className="bg-[#06C755] h-[300px] md:flex items-center mt-[35px] ">
-              <FaLine className="md:w-[200px] md:h-[200px] md:ml-[56px] w-[100px] ml-auto mr-auto h-[100px]" />
+            <div className="bg-[#06C755] md:h-[300px] h-fit  md:flex items-center md:mt-[125px] mt-[35px] p-5 ">
+              <FaLine className="md:w-[200px] md:h-[200px] md:ml-[56px] w-[100px] max-[376px]:ml-auto max-[376px]:mr-auto h-[100px]" />
               <h1 className="text-white md:text-[74px] md:ml-[182px] text-[24px] text-center">
                 lantongshop
               </h1>
@@ -41,8 +41,8 @@ export default function ContactUs() {
 
           {/* Email */}
           <Link href="#">
-            <div className="bg-[#FF5A60] h-[300px] md:flex items-center mt-[35px] ">
-              <HiOutlineMail className="md:w-[200px] md:h-[200px] md:ml-[56px] ml-auto mr-auto w-[100px] h-[100px]" />
+            <div className="bg-[#FF5A60] md:h-[300px] h-fit  md:flex items-center md:mt-[125px] mt-[35px] p-5 ">
+              <HiOutlineMail className="md:w-[200px] md:h-[200px] md:ml-[56px] max-[376px]:ml-auto max-[376px]:mr-auto w-[100px] h-[100px]" />
               <h1 className="text-white md:text-[65px] text-[24px] md:ml-[182px] md:mr-[24px] text-center">
                 lantongshop@gmail.com
               </h1>
