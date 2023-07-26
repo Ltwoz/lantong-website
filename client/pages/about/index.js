5ee11c7163db7a7495214f3aca227159507cda4a
ZwaneProgram <phuwasit.wantaya@gmail.com>
2023-07-26 20:22:24 +0700

    add-some-bg

--- client/pages/about/index.js
+++ client/pages/about/index.js
@@ -1,9 +1,8 @@
-
 import Layout from "@/components/layouts/Layout";
 
 export default function About() {
   return (
-    <Layout>
+    <Layout style={{ backgroundSize: "1920px 800px", background: "url('/path/to/background-image.jpg') no-repeat center center fixed" }}>
     </Layout>
   );
 }
