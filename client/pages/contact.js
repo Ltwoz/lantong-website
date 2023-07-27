f1b82fc1e1574c9de9d8128f32202d63f5bf24b4
JarYin <tempest353@hotmail.com>
2023-07-27 21:37:11 +0700

    update contact.js

--- client/pages/contact.js
+++ client/pages/contact.js
@@ -8,79 +8,81 @@ import Link from "next/link";
 export default function ContactUs() {
   return (
     <Layout>
-      <div className="md:w-[1200px] mx-auto md:flex md:border rounded shadow md:p-8 w-full">
+      <div className="md:w-[1200px] mx-auto md:flex md:border rounded shadow md:p-8 w-full px-4 gap-10">
         <div className="md:ml-8 ">
-          <h1 className="text-[36px] max-[375px]:text-center font-semibold text-[#B53326]">
+          <h1 className="text-[36px] md:text-start text-center font-semibold text-[#B53326]">
             ติดต่อเรา
           </h1>
 
-          <div className="">
-            <p className="bg-[#4E4E4E] md:w-[200px] text-white md:h-[50px] justify-center items-center text-[24px] rounded-[6px] flex ">
-              <AiOutlinePhone className="mr-2 rotate-90" />
-              081 952 1342
-            </p>
-          </div>
-
-          {/* Facebook */}
-          <Link href={"#"}>
-            <div className="md:hover:bg-slate-50 max-[375px]:text-center max-[375px]:bg-[#4267B2]">
-              <h1 className="mt-8 font-bold md:text-[18px] text-[28px] text-[#4267B2] max-[375px]:text-white">
-                Facebook
-              </h1>
-
-              <div className="flex items-center max-[375px]:justify-center max-[375px]:flex-col mt-2">
-                <BsFacebook className="md:w-[50px] md:h-[50px] mr-2 w-[100px] h-[100px]" />
-                <p className="font-semibold max-[375px]:text-[24px] text-[#4267B2] mt-2 max-[375px]:text-white">
-                  ลานทองพ่วงข้าง
-                </p>
-              </div>
-            </div>
-          </Link>
-
-          {/* Line */}
-          <Link href={"#"}>
-            <div className="md:hover:bg-slate-50 max-[375px]:text-center max-[375px]:bg-[#06C755]">
-              <h1 className="mt-8 font-bold md:text-[18px] text-[28px] text-[#06C755] max-[375px]:text-white">
-                Line
-              </h1>
-              <div className="flex items-center max-[375px]:justify-center max-[375px]:flex-col mt-2">
-                <FaLine className="md:w-[50px] md:h-[50px] md:mr-2  w-[100px] h-[100px]" />
-                <p className="font-semibold max-[375px]:text-[24px] md:text-[#06C755] text-white">
-                  ลานทองพ่วงข้าง
-                </p>
-              </div>
-            </div>
-          </Link>
-
-          {/* Email */}
-          <Link href={"#"}>
-            <div className="md:hover:bg-slate-50 max-[375px]:text-center max-[375px]:bg-[#FF5A60]">
-              <h1 className="mt-8 font-bold md:text-[18px] text-[28px] text-[#FF5A60] max-[375px]:text-white">
-                Email us
-              </h1>
-              <div className="flex items-center max-[375px]:justify-center max-[375px]:flex-col mt-2">
-                <HiOutlineMail className="md:w-[50px] md:h-[50px] md:mr-2 w-[100px] h-[100px]" />
-                <p className="font-semibold md:text-[#FF5A60] text-white max-[375px]:text-[24px]">
-                  lantongshop@gmail.com
-                </p>
-              </div>
-            </div>
-          </Link>
-
-          <div className="mt-8">
-            <h1 className="font-semibold">
-              ห้างหุ้นส่วนจำกัด ลานทองธุรกิจยนต์
-            </h1>
-            <div className="flex items-center">
-              <FaRoad className="md:w-[50px] md:h-[50px] w-[100px] h-[100px] mr-2 md:mr-2 mt-2" />
-              <p>
-                4/4 ถ.รัตนโกสินทร์ ต.ช้างม่อย อ.เมืองเชียงใหม่ จ.เชียงใหม่ 50200
+          <div className="flex flex-col gap-4">
+            <div className="">
+              <p className="bg-[#4E4E4E] md:w-[200px] text-white md:h-[50px] justify-center items-center text-[24px] rounded-md flex ">
+                <AiOutlinePhone className="mr-2 rotate-90" />
+                081 952 1342
               </p>
+            </div>
+            {/* Facebook */}
+            <Link href={"#"}>
+              <div className="md:hover:bg-slate-50 md:text-start text-center md:bg-transparent bg-[#4267B2] rounded-md">
+                <h1 className=" font-bold md:text-[18px] text-[28px] md:text-[#4267B2] text-white">
+                  Facebook
+                </h1>
+
+                <div className="flex items-center md:justify-start justify-center md:flex-row flex-col mt-2">
+                  <BsFacebook className="md:w-[50px] md:h-[50px] mr-2 w-[100px] h-[100px]" />
+                  <p className="font-semibold text-[24px] md:text-[#4267B2] mt-2 text-white">
+                    ลานทองพ่วงข้าง
+                  </p>
+                </div>
+              </div>
+            </Link>
+
+            {/* Line */}
+            <Link href={"#"}>
+              <div className="md:hover:bg-slate-50 md:text-start text-center md:bg-transparent bg-[#06C755] rounded-md">
+                <h1 className="font-bold md:text-[18px] text-[28px] md:text-[#06C755] text-white">
+                  Line
+                </h1>
+                <div className="flex items-center md:justify-start justify-center md:flex-row flex-col mt-2">
+                  <FaLine className="md:w-[50px] md:h-[50px] md:mr-2  w-[100px] h-[100px]" />
+                  <p className="font-semibold text-[24px] md:text-[#06C755] text-white">
+                    ลานทองพ่วงข้าง
+                  </p>
+                </div>
+              </div>
+            </Link>
+
+            {/* Email */}
+            <Link href={"#"}>
+              <div className="md:hover:bg-slate-50 md:text-start text-center md:bg-transparent bg-[#FF5A60] rounded-md">
+                <h1 className="font-bold md:text-[18px] text-[28px] md:text-[#FF5A60] text-white">
+                  Email us
+                </h1>
+                <div className="flex items-center md:justify-start justify-center md:flex-row flex-col mt-2">
+                  <HiOutlineMail className="md:w-[50px] md:h-[50px] md:mr-2 w-[100px] h-[100px]" />
+                  <p className="font-semibold md:text-[#FF5A60] text-white text-[24px]">
+                    lantongshop@gmail.com
+                  </p>
+                </div>
+              </div>
+            </Link>
+
+            <div className="flex flex-col">
+              <h1 className="font-semibold md:text-[18px]">
+                Address
+              </h1>
+              <div className="flex items-center">
+                <FaRoad className="md:w-[50px] md:h-[50px] w-[100px] h-[100px] mr-2 md:mr-2 mt-2" />
+                <p>
+                ห้างหุ้นส่วนจำกัด ลานทองธุรกิจยนต์ 4/4 ถ.รัตนโกสินทร์ ต.ช้างม่อย อ.เมืองเชียงใหม่ จ.เชียงใหม่
+                  50200
+                </p>
+              </div>
             </div>
           </div>
         </div>
-        <hr/>
-        <div className="md:w-full w-[375px] mt-4">
+        <hr className="md:hidden"/>
+        <div className="w-full mt-4">
           <h1 className="text-center">GOOGLE MAP</h1>
           <div className="bg-slate-500 h-[500px]"></div>
         </div>
