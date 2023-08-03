60aa3c2b0bc6925041b592ff49efe3fbcc19b2f4
Ltwoz <warrapat.102@gmail.com>
2023-08-03 14:44:10 +0700

    add cookie-parser

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("./utils/db");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 const PORT = process.env.PORT || 8000;
@@ -24,6 +25,7 @@ dotenv.config({ path: ".env.local" });
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb" }));
+app.use(cookieParser());
 app.use(cors(corsOptions));
 
 connectDB().then(() => {
