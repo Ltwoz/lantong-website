1fca0bd2a20481af4731d103a8fdb75464677877
Ltwoz <warrapat.102@gmail.com>
2023-08-09 22:03:30 +0700

    routes and handler

--- server/api/website/website.handler.js
+++ server/api/website/website.handler.js
@@ -2,7 +2,20 @@ const catchAsyncErrors = require("../../middleware/catchAsyncErrors");
 const Website = require("./website.model");
 
 exports.getWebsiteConfig = catchAsyncErrors(async (req, res, next) => {
-    const config = Website.find();
+    const config = Website.findOne().select("-__v -_id");
 
     res.status(200).json({ success: true, config });
-})
+});
+
+exports.updateWebsiteConfig = catchAsyncErrors(async (req, res, next) => {
+    const config = await Website.findOneAndUpdate(
+        {},
+        { $set: req.body },
+        { upsert: true }
+    );
+
+    res.status(201).json({
+        success: true,
+        config,
+    });
+});
