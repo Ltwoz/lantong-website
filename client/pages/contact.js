77bde3c204bece418bd0d1dd84706c2957cb8607
Ltwoz <warrapat.102@gmail.com>
2023-08-24 21:45:38 +0700

    update contact

--- client/pages/contact.js
+++ client/pages/contact.js
@@ -8,31 +8,20 @@ import Link from "next/link";
 export default function ContactUs({ config }) {
     return (
         <Layout>
-            <section className="mx-auto max-w-[1200px] px-4 xl:px-0 flex flex-col md:flex-row gap-4 xl:gap-6 py-10">
-                <div className="mx-auto xl:flex xl:border xl:rounded xl:shadow xl:p-8 w-full px-4 gap-10">
+            <section className="mx-auto max-w-[1200px] px-4 xl:px-0 flex flex-col md:flex-row py-10">
+                <div className="flex flex-col xl:flex-row xl:border xl:rounded xl:shadow xl:p-8 w-full px-4 gap-6 h-fit">
                     <div className="flex flex-col gap-4">
                         <h1 className="text-[36px] xl:text-start text-center font-semibold">
                             ติดต่อเรา
                         </h1>
-
                         <div className="flex flex-col gap-4">
-                            <div className="gap-4 flex flex-col xl:flex-row justify-between">
-                                <p className="bg-[#4E4E4E] w-full text-white xl:h-[50px] justify-center items-center text-[24px] rounded-md flex ">
-                                    <AiOutlinePhone className="mr-2 rotate-90" />
-                                    053 876 109
-                                </p>
-                                <p className="bg-[#4E4E4E] w-full text-white xl:h-[50px] justify-center items-center text-[24px] rounded-md flex ">
-                                    <AiOutlinePhone className="mr-2 rotate-90" />
-                                    081 952 1342
-                                </p>
-                            </div>
                             {/* Facebook */}
                             <Link
                                 href={config.social?.facebook_url}
-                                className="flex flex-row p-4 gap-3 hover:bg-slate-50 xl:rounded-md"
+                                className="flex flex-row p-4 gap-3 bg-[#4267B2] xl:bg-white hover:bg-slate-100 rounded-md"
                             >
                                 <div className="flex items-center justify-center">
-                                    <BsFacebook className="xl:w-[50px] xl:h-[50px] mr-2 w-[100px] h-[100px] xl:text-[#4267B2] text-white" />
+                                    <BsFacebook className="mr-2 w-[50px] h-[50px] xl:text-[#4267B2] text-white" />
                                 </div>
                                 <div className="flex flex-col">
                                     <h3 className=" font-bold text-base xl:text-[#4267B2] text-white">
@@ -47,10 +36,10 @@ export default function ContactUs({ config }) {
                             {/* Line */}
                             <Link
                                 href={config.social?.line_url}
-                                className="flex flex-row p-4 gap-3 hover:bg-slate-50 xl:rounded-md"
+                                className="flex flex-row bg-[#06C755] xl:bg-white p-4 gap-3 hover:bg-slate-100 rounded-md"
                             >
                                 <div className="flex items-center justify-center">
-                                    <FaLine className="xl:w-[50px] xl:h-[50px] mr-2 w-[100px] h-[100px] xl:text-[#06C755] text-white" />
+                                    <FaLine className="mr-2 w-[50px] h-[50px] xl:text-[#06C755] text-white" />
                                 </div>
                                 <div className="flex flex-col">
                                     <h3 className=" font-bold text-base xl:text-[#06C755] text-white">
@@ -65,10 +54,10 @@ export default function ContactUs({ config }) {
                             {/* Email */}
                             <Link
                                 href={"mailto:lantongshop@gmail.com"}
-                                className="flex flex-row p-4 gap-3 hover:bg-slate-50 xl:rounded-md"
+                                className="flex flex-row bg-[#FF5A60] xl:bg-white p-4 gap-3 hover:bg-slate-100 rounded-md"
                             >
                                 <div className="flex items-center justify-center">
-                                    <HiOutlineMail className="xl:w-[50px] xl:h-[50px] mr-2 w-[100px] h-[100px] xl:text-[#FF5A60] text-white" />
+                                    <HiOutlineMail className="mr-2 w-[50px] h-[50px] xl:text-[#FF5A60] text-white" />
                                 </div>
                                 <div className="flex flex-col">
                                     <h3 className=" font-bold text-base xl:text-[#FF5A60] text-white">
@@ -82,10 +71,10 @@ export default function ContactUs({ config }) {
 
                             <Link
                                 href={"#"}
-                                className="flex flex-row p-4 gap-3 hover:bg-slate-50 xl:rounded-md"
+                                className="flex flex-row bg-cyan-700 xl:bg-white p-4 gap-3 hover:bg-slate-100 rounded-md"
                             >
                                 <div className="flex items-start justify-center">
-                                    <HiOutlineOfficeBuilding className="xl:w-[50px] xl:h-[50px] mr-2 w-[100px] h-[100px] xl:text-cyan-700 text-white" />
+                                    <HiOutlineOfficeBuilding className="mr-2 w-[50px] h-[50px] xl:text-cyan-700 text-white" />
                                 </div>
                                 <div className="flex flex-col">
                                     <h3 className=" font-bold text-base xl:text-cyan-700 text-white">
@@ -97,12 +86,23 @@ export default function ContactUs({ config }) {
                                     </p>
                                 </div>
                             </Link>
+                            <div className="gap-4 flex flex-col xl:flex-row justify-between">
+                                <p className="bg-[#4E4E4E] w-full text-white justify-center items-center text-xl rounded-md flex flex-row px-4 py-3">
+                                    <AiOutlinePhone className="mr-2 rotate-90" />
+                                    053 876 109
+                                </p>
+                                <p className="bg-[#4E4E4E] w-full text-white justify-center items-center text-xl rounded-md flex flex-row px-4 py-3">
+                                    <AiOutlinePhone className="mr-2 rotate-90" />
+                                    081 952 1342
+                                </p>
+                            </div>
                         </div>
                     </div>
+
                     <hr className="xl:hidden" />
-                    <div className="w-full mt-4">
-                        <h1 className="text-center">GOOGLE MAP</h1>
-                        <div className="bg-slate-500 h-[500px]"></div>
+
+                    <div className="w-full">
+                        <div className="bg-slate-500 xl:h-full"></div>
                     </div>
                 </div>
             </section>
