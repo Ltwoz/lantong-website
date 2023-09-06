b9e111e827fc8212ff8407d611d3591b84e8264d
Ltwoz <warrapat.102@gmail.com>
2023-09-06 19:10:53 +0700

    add gtag

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -3,12 +3,47 @@ import { DashboardSidebarContextProvider } from "@/contexts/dashboard-sidebar-co
 import { SidebarContextProvider } from "@/contexts/sidebar-context";
 import { UserContextProvider } from "@/contexts/user-context";
 import "@/styles/globals.css";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import * as gtag from "@/utils/gtag";
+import Script from "next/script";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
     const { config } = pageProps;
+    const router = useRouter();
+
+    useEffect(() => {
+        const handleRouteChange = (url) => {
+            gtag.pageview(url);
+        };
+        router.events.on("routeChangeComplete", handleRouteChange);
+        return () => {
+            router.events.off("routeChangeComplete", handleRouteChange);
+        };
+    }, [router.events]);
 
     return (
         <>
+            <Head>
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+                            gtag('config', '${gtag.GA_TRACKING_ID}', {
+                                page_path: window.location.pathname,
+                            });
+                        `,
+                    }}
+                />
+            </Head>
+            {/* Global Site Tag (gtag.js) - Google Analytics */}
+            <Script
+                strategy="afterInteractive"
+                src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+            />
             <ConfigContextProvider value={config}>
                 <UserContextProvider>
                     <DashboardSidebarContextProvider>
